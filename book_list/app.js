835fbf56a95c9ea675166afe0df3923d95211bcb
OzSlapak <ozgeslapak@gmail.com>
2022-09-09 17:14:11 -0400

    debugged

--- book_list/app.js
+++ book_list/app.js
@@ -6,24 +6,24 @@ const bookListEl = document.getElementById("book-list")
 
 const book ={title:"", author:"", isbn:""}
 
-formEl.addEventListener("sucmit",(e) =>{
+formEl.addEventListener("submit",(e) =>{
     e.preventDefault()
     book["title"] = titleEl.value
     book["author"] = authorEl.value
     book["isbn"] = isbn.value
     console.log(book);
     const row= document.createElement("tr")
-    row.innerHTML= `<td>${book[title]}</td><td>${book["author']</td><td>${book[isbn]}</td><td><span class="delete">X</span></td>`
+    row.innerHTML= `<td>${book["title"]}</td><td>${book["author"]}</td><td>${book["isbn"]}</td><td><span class="delete">X</span></td>`
     bookListEl.appendChild(row)
 
     titleEl.value = ""
-    authorEl.value =""
-    isbnEl.value =""
+    authorEl.value = ""
+    isbnEl.value = ""
 })
 
 bookListEl.addEventListener("click",(e) => {
-    console.log(e, target);
-    if(e.target.className ==="delete"){
+    console.log(e.target);
+    if(e.target.className === "delete"){
         e.target.parentElement.parentElement.remove()
     }
 })
